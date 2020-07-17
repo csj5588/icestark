dcb49cbce1164358f37f0eab3c42b1cd5b030817
cuishijie <cuishijie@inke.cn>
2020-07-17 09:27:25 +0800

    feat:删除冗余

--- apps/react-icestark-child-16/src/store/action-types.js
+++
@@ -1,4 +0,0 @@
-export const STARK_EMIT_DOWN = 'STARK_EMIT_DOWN'
-export const STARK_EMIT_UP = 'STARK_EMIT_UP'
-// DEMO
-export const GET_DEMO = 'GET_DEMO'
--- apps/react-icestark-child-16/src/store/action.js
+++
@@ -1,3 +0,0 @@
-import * as types from './action-types'
-
-const getDemo = payload => ({ type: types.GET_DEMO, payload })
