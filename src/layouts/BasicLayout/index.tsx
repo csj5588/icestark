9999906c8f1d65502119e55a5ce3c1788dea3418
cuishijie <cuishijie@inke.cn>
2020-07-22 11:51:56 +0800

    feat: layout header布局暂存

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Shell } from '@alifd/next';
-import { AppLink, appHistory } from '@ice/stark';
+import { AppLink } from '@ice/stark';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import _isEmpty from 'lodash/isEmpty';
+import { connect } from 'react-redux'
+import _isEmpty from 'lodash/isEmpty'
 import { asideMenuConfig } from './menuConfig';
 import { getDataAuth } from '@/entry/apis';
-import Cookie from '@/utils/cookies';
-import * as actions from '@/store/action';
+import Cookie from '@/utils/cookies'
+import * as actions from '@/store/action'
 import Footer from './components/Footer';
 import Header from './components/Header/Header';
 import S from './apis';
-import { AUTH, DOCUMENT, CONTROL, MANAGE, ORDER } from './constant';
 
-import 'antd/dist/antd.less';
+import 'antd/dist/antd.css';
 import './index.scss';
 
 const saveAppKey = 'cur-auth-app';
-const ALL_AUTH = { appid: 'all', name: '全部' };
-const ALL = 'all'; // 全部app权限
+const ALL_AUTH = { appid: 'all', name: '全部' }
+const ALL = 'all' // 全部app权限
 
 declare global {
   interface Window {
@@ -29,127 +28,69 @@ declare global {
 class BasicLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      current: CONTROL,
-    };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.getDataAuth();
     this.initActions();
   }
 
-  initActions() {
-    const { dispatch } = this.props;
-    dispatch(actions.getPageButtonTree());
-    dispatch(actions.getPageButtonTreeUid());
+  initActions () {
+    const { dispatch } = this.props
+    dispatch(actions.getPageButtonTree())
+    dispatch(actions.getPageButtonTreeUid())
   }
 
   // 设置select 默认选中的app
   getDefaultApp(list) {
-    const app = Cookie.getItem(saveAppKey);
-    if (list.findIndex((x) => x.appid === app) !== -1) {
-      return app;
+    let app = Cookie.getItem(saveAppKey)
+    if (list.findIndex(x => x.appid === app) !== -1) {
+      return app
     }
-    return list[0] && list[0].appid;
+    return list[0] && list[0].appid
   }
 
-  getDefaultAppItem(list) {
-    let app = Cookie.getItem(saveAppKey);
-    const index = list.findIndex((x) => x.appid === app)
-    if ( index !== -1) {
-      return list[index];
-    }
-    return list[0] || {};
-  }
-
-  getDataAuth() {
-    const { dispatch } = this.props;
-    getDataAuth({}).then(async ({ data }) => {
-      const { data: appList } = await S.getAppList();
-      const dataAuth = data.data_power_tree.data_power;
-      const { apps = [] } = dataAuth;
-      const appListOptions = apps.includes(ALL)
-        ? appList
-        : appList.filter((item) => apps.indexOf(item.appid) >= 0);
-      const defaultApp = this.getDefaultApp(appList);
-      const defaultAppItem = this.getDefaultAppItem(appList);
-      Cookie.setItem(saveAppKey, defaultApp);
-      // 判断有没有当前app的权限
-      const hasAppAuth = appListOptions.some(app => app.appid === defaultApp)
-      this.setState({
-        dataAuthComplete: true,
-        defaultApp,
-        appListOptions,
-      });
-      const authList = [ALL_AUTH, ...appList];
-      dispatch(
-        actions.setAuthAppList({
+  getDataAuth () {
+    const { dispatch } = this.props
+    getDataAuth({})
+      .then(async ({ data }) => {
+        const { data: appList } = await S.getAppList()
+        const dataAuth = data.data_power_tree.data_power
+        const { apps = [] } = dataAuth
+        const appListOptions = (apps.includes(ALL)) ? appList : appList.filter(item => (apps.indexOf(item.appid) >= 0))
+        const defaultApp = this.getDefaultApp(appList);
+        Cookie.setItem(saveAppKey, defaultApp);
+        this.setState({
+          dataAuthComplete: true,
+          defaultApp,
+          appListOptions
+        })
+        const authList = [ALL_AUTH, ...appList]
+        dispatch(actions.setAuthAppList({
           curApp: defaultApp,
-          hasAppAuth,
           appList: appListOptions,
           authList,
-          curAppItem: defaultAppItem,
-        })
-      );
-    });
+        }))
+      })
   }
 
-  handChangeRoute = () => {
-    this.initActions();
-  };
-
   renderChildMenu = () => {
-    const { pathname, treeRight } = this.props;
-    const menuChildren = this.formatAuthMenu(
-      pathname,
-      AUTH,
-      treeRight,
-      asideMenuConfig
-    );
-    return menuChildren;
-  };
+    const { pathname } = this.props;
 
-  //  判断权限管理内的子页面
-  formatAuthMenu = (pathname, target, treeRight, asideMenuConfig) => {
-    const menuChild =
-      asideMenuConfig.find((x) => {
-        const [name, ...other] = pathname.split('-');
-        return x.path.includes(name);
-      }) || {};
-    const [treeRightItem, ...otr] = treeRight;
-    const { children: authChildren } = treeRightItem || {};
-    const auth =
-      (authChildren && authChildren.find((x) => x.root === AUTH)) || {};
-    const authMenuChildren =
-      menuChild.children &&
-      menuChild.children.filter((x) => {
-        const [name, root] = x.path.split('-');
-        const filterItem =
-          auth.children &&
-          auth.children.find((y) => {
-            return y.root === root;
-          });
-        return filterItem;
-      });
-    const menuChildren = authMenuChildren || [];
+    const menuChild = asideMenuConfig.find(x => {
+      const [name, ...other] = pathname.split('-')
+      return x.path.includes(name);
+    }) || {};
+    const menuChildren = menuChild.children || [];
     return menuChildren;
-  };
-
-  // 判断是否有管理权限
-  getManageAuth = () => {
-    const { treeRight } = this.props;
-    const [treeRightItem, ...otr] = treeRight;
-    const { children: authChildren } = treeRightItem || {};
-    const hasManageAuth = authChildren.some(item => item.root === AUTH)
-    return hasManageAuth
   }
 
   render() {
-    const { children, pathname, appToken } = this.props;
+    const { children, pathname } = this.props;
     const menuChildren = this.renderChildMenu();
-    const needSideMenu = !_isEmpty(menuChildren);
-    const hasManageAuth = this.getManageAuth()
+
+    const needSideMenu = !_isEmpty(menuChildren)
+
     return (
       <Shell
         type="brand"
@@ -159,56 +100,52 @@ class BasicLayout extends React.Component {
       >
         <Shell.Branding className="layout-menu-top">
           <div className="layout-logo">
-            <img
-              src="https://img.ikstatic.cn/MTU5MjI5MDQ4MTY3OSMgOTMjcG5n.png"
-              alt=""
-            />
+            <img src="https://img.ikstatic.cn/MTU5MjI5MDQ4MTY3OSMgOTMjcG5n.png" alt=""/>
           </div>
           <div className="layout-menu-top-item">
-            <div className={`layout-menu-top-items`}>
-              { hasManageAuth ? <AppLink
-                to="/auth-userMs"
-                className={`layout-menu-top-manage ${
-                  pathname.includes(MANAGE) ? 'act' : ''
-                }`}
-              >
-                系统管理
-              </AppLink> : null }
-              <AppLink
-                to="/vue-app"
-                className={`layout-menu-top-order ${
-                  pathname.includes(ORDER) ? 'act' : ''
-                }`}>
-                工单
-              </AppLink>
-            </div>
+            {
+              asideMenuConfig.map((item, idx) => {
+                return (
+                  <AppLink
+                    key={idx}
+                    to={item.path}
+                    className={`layout-menu-top-items`}
+                  >
+                    {item.name}
+                  </AppLink>
+                )
+              })
+            }
           </div>
           <Header />
         </Shell.Branding>
 
         <Shell.Content>
-          <div className={`layout-content  ${pathname.includes(MANAGE) ? 'auth-manage' : ''}`}>
-            {needSideMenu ? (
-              <div className="side-menu">
-                <Router>
-                  {menuChildren.map((item, idx) => {
-                    return (
-                      <Link
-                        key={idx}
-                        onClick={this.handChangeRoute}
-                        to={item.path}
-                        className={`layout-menu-top-items ${
-                          pathname === item.path ? 'act' : ''
-                        }`}
-                      >
-                        {item.name}
-                      </Link>
-                    );
-                  })}
-                </Router>
-              </div>
-            ) : null}
-            <div className="layout-content-router">{children}</div>
+          <div className="layout-content">
+            {
+              needSideMenu ? (
+                <div className="side-menu">
+                  <Router>
+                    {
+                      menuChildren.map((item, idx) => {
+                        return (
+                          <Link
+                            key={idx}
+                            to={item.path}
+                            className={`layout-menu-top-items ${pathname === item.path ? 'act' : ''}`}
+                          >
+                            {item.name}
+                          </Link>
+                        )
+                      })
+                    }
+                  </Router>
+                </div>
+              ) : null
+            }
+            <div className="layout-content-router">
+              {children}
+            </div>
           </div>
         </Shell.Content>
         <Shell.Footer>
@@ -219,8 +156,7 @@ class BasicLayout extends React.Component {
   }
 }
 
-export default connect((stores) => ({
+export default connect(stores => ({
   appToken: stores.authApp.curApp,
   authApp: stores.authApp,
-  treeRight: stores.auth.pageButtonTreeRight,
-}))(BasicLayout);
+}))(BasicLayout)
